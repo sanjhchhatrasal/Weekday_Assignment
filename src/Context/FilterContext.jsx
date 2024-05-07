8a46a889e82cecb89bbaa1698c9e09375a6ae2e6
Sanjh Chhatrasal <sanjhchhatrasal@gmail.com>
2024-05-07 15:34:01 +0530

    Built FilterContext and FilterReducer

---
+++ src/Context/FilterContext.jsx
@@ -0,0 +1,53 @@
+import { createContext, useContext, useEffect, useReducer } from "react";
+import { useCardContext } from "./CardContext";
+import reducer from "../Reducer/FilterReducer";
+
+
+const FilterContext = createContext();
+
+const initialState = {
+    filter_jobs: [],
+    all_jobs: [],
+    filters : {
+        text: "",
+    },
+};
+
+export const FilterContextProvider = ({ children }) => {
+    const { jobs } = useCardContext();
+    const [ state, dispatch] = useReducer(reducer, initialState);
+
+
+   // update the filter values
+  const updateFilterValue = (event) => {
+    let name = event.target.name;
+    let value = event.target.value;
+
+    return dispatch({ type: "UPDATE_FILTERS_VALUE", payload: { name, value } });
+  };
+  
+  //to filter on basis of input
+  useEffect(() => {
+    dispatch({ type: "FILTER_PRODUCTS", payload: state.filters.text})
+  }, [jobs, state.filters.text]);
+  
+  
+    useEffect(() => {
+      dispatch( { type: "LOAD_FILTER_PRODUCTS", payload: jobs } );
+    }, [jobs]);
+    
+
+    return (
+        <FilterContext.Provider value={{ ...state, updateFilterValue }}>
+            {children}
+        </FilterContext.Provider>
+    );
+};
+
+ const useFilterContext = () => {
+    return useContext(FilterContext);
+};
+
+export { useFilterContext}
+
+ 
---
+++ src/Reducer/FilterReducer.js
@@ -0,0 +1,62 @@
+const filterReducer = (state, action) => {
+  switch (action.type) {
+    case "LOAD_FILTER_PRODUCTS":
+      return {
+        ...state,
+        filter_jobs: Array.isArray(action.payload)
+          ? [...action.payload]
+          : state.filter_jobs,
+        all_jobs: Array.isArray(action.payload)
+          ? [...action.payload]
+          : state.all_jobs,
+        /*   filter_jobs: [...action.payload],
+                 all_jobs: [...action.payload],  */
+      };
+
+    case "UPDATE_FILTERS_VALUE":
+      const { name, value } = action.payload;
+
+      return {
+        ...state,
+        filters: {
+          ...state.filters,
+          [name]: value,
+        },
+      };
+
+    case "FILTER_PRODUCTS":
+      console.log("Payload type:", typeof action.payload); // Add this line to check the type of action.payload
+
+      let { all_jobs } = state;
+      let tempFilterJob = [...all_jobs];
+
+      const { text } = state.filters;
+
+      if (text) {
+        tempFilterJob = tempFilterJob.filter((job) => {
+          console.log(" before Filtered Jobs:", tempFilterJob);
+          if (job.companyName && typeof job.companyName === "string") {
+            return job.companyName.toLowerCase().includes(text.toLowerCase());
+          }
+          return false; // If job.companyName is undefined or not a string, exclude it from the filter
+        });
+        /*  if (job.companyName && typeof job.companyName === 'string') {
+                                      return job.companyName.toLowerCase().includes(text.toLowerCase());
+                                    }
+                                    
+                                  }; */
+      }
+      console.log("All Jobs:", all_jobs); // Add this line to log all_jobs value
+      console.log("Filtered Jobs:", tempFilterJob);
+
+      return {
+        ...state,
+        filter_jobs: tempFilterJob,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default filterReducer;
